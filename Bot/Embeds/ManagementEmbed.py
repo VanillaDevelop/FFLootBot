8630b563c7c7fa2f34c7896caf89e70392ab61dd
Vanilla <vanilla@nya.gg>
2022-07-25 23:57:21 +0200

    Subclass Embed and View instead of writing them inline, btns => selects

---
+++ Bot/Embeds/ManagementEmbed.py
@@ -0,0 +1,26 @@
+import discord
+from Bot.Team import Team
+
+
+class ManagementEmbed(discord.Embed):
+    def __init__(self, team: Team, COMMAND_PREFIX: str, uuid: str):
+        self.title(name="Team Management")
+        self.set_author(f"Team {team.name}")
+        self.description = f"This is the control panel for team {team.name}."
+        self.add_field(name="Inviting Team Members", value=f"Team members may join the team by DMing the bot with "
+                                                           f"**{COMMAND_PREFIX}join {uuid}**.", inline=False)
+        self.add_field(name="Loot Distribution", value="Select below who should get priority for loot drops."
+                                                       "\n**DPS**\n"
+                                                       "Loot will be distributed according to the biggest DPS "
+                                                       "improvement. This means loot will go exclusively to DPS "
+                                                       "(unless all DPS already possess a similar or better item)"
+                                                       " and large item upgrades will be prioritized."
+                                                       "\n**Equal Loot**\n"
+                                                       "Loot will be distributed in such "
+                                                       "a way that the power increase of all players is roughly "
+                                                       "equal. Players who have less loot will receive priority "
+                                                       "on drops which benefit them."
+                                                       "\n**None**\n"
+                                                       "No loot priority will be provided, but the bot will "
+                                                       "show the potential upgrade for every player.",
+                       inline=False)
